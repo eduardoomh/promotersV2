e69ddf8d02b466c6a4bb76652efdd0a6815f5869
eduardoomh <eduardoomh@outlook.com>
2023-10-30 01:06:21 -0500

    fix - login redirect when the user is promoter

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -41,7 +41,7 @@ const LoginForm = () => {
 
             updateUserData(result.data.userLogged)
             if (result.data.userLogged.role === 'promoter') {
-                router.push('/perfil')
+                router.push(`/perfil/${result.data.userLogged._id}`)
             } else {
                 router.push('/')
             }
